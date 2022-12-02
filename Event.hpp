2cdf99639eab5603d936234ccebb5270a87a516a
Paweł Rapacz <apacz_paw@wp.pl>
2022-12-02 14:56:16 +0100

    LICENSE update (headers in every file, and little --version modification)

--- Event.hpp
+++ Event.hpp
@@ -1,3 +1,23 @@
+/**
+ * daterem - a program that helps you keep track your plans and reminds
+ * you your daily things.
+ * Copyright (C) 2022  Paweł Rapacz
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
 #pragma once
 
 #include <windows.h>
--- main.cpp
+++ main.cpp
@@ -1,3 +1,23 @@
+/**
+ * daterem - a program that helps you keep track your plans and reminds
+ * you your daily things.
+ * Copyright (C) 2022  Paweł Rapacz
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
 #include <iostream>
 #include <vector>
 #include <windows.h>
@@ -30,7 +50,7 @@ int main(int argc, char *argv[])
         {
             if (argc != 2) dr::ArgErr();
 
-            std::cout << "daterem 1.0.0\nCopyright (C) 2022 Paweł Rapacz\nThis program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.\nThis is free software, and you are welcome to redistribute it\nunder certain conditions; type `show c' for details.";
+            std::cout << "daterem 1.0.0\nCopyright (C) 2022 Paweł Rapacz\nThis program comes with ABSOLUTELY NO WARRANTY;\nThis is free software, and you are welcome to redistribute it\nunder certain conditions.";
             return EXIT_SUCCESS;
         }
 
